400925832300594b6e4475318e62b2f44279237a
NovaPrime1 <corey.powe@gmail.com>
2023-09-26 16:57:11 -0400

    Change for livereload

--- src/main/java/com/recipe/project/webdevspringmvc/controllers/IndexController.java
+++ src/main/java/com/recipe/project/webdevspringmvc/controllers/IndexController.java
@@ -9,7 +9,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "index"})
     public String getIndexPage(){
-        System.out.println(" Some message to say...1234564646");
+//        System.out.println(" Some message to say...1234564646");
         return "index";
     }
 }
